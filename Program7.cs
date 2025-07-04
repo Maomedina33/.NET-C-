9bc9512a0c2e140f437a67f4739a7d5cdfd13485
Maomedina33 <ingmaomedina@gmail.com>
2025-07-04 12:18:40 -0500

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,24 @@
+﻿List<string> nombres2 = new List<string>();
+nombres2.Add("oscar");
+nombres2.Add("Felipe");
+nombres2.Add("Amin");
+nombres2.Add("Jorge");
+
+switch (nombres2[2])
+{
+    case "oscar":
+        Console.WriteLine("El nombre es oscar");
+        break;
+    case "Felipe":
+        Console.WriteLine("El nombre es Felipe");
+        break;
+    case "Amin":
+        Console.WriteLine("El nombre es Amin");
+        break;
+    case "Jorge":
+        Console.WriteLine("El nombre es Jorge");
+        break;
+    default:
+        Console.WriteLine("No se encontró el nombre"); break;
+        
+}
