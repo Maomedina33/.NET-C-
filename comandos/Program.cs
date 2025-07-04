5dbfac1addf736fadabf786b7376889801333027
Maomedina33 <ingmaomedina@gmail.com>
2025-07-04 12:27:18 -0500

    Add files via upload

---
+++ comandos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using Newtonsoft.Json;
+
+public class person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }        
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new person
+        {
+            Name = "John Doe",
+            Age = 30
+        };
+
+
+        string json = JsonConvert.SerializeObject(person);
+        Console.WriteLine("Serialezed JSON:" + json);
+
+        var deserializedPerson = JsonConvert.DeserializeObject<person>(json);
+        Console.WriteLine($"Deserialized Person: Name = {deserializedPerson.Name}, Age = {deserializedPerson.Age}");
+    }
+}
+
