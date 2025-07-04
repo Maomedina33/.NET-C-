dc0c96db83c03c740a9bae1cd8bcc86fc2a433cd
Maomedina33 <ingmaomedina@gmail.com>
2025-07-04 12:34:25 -0500

    Add files via upload

---
+++ comandos/Program3.cs
@@ -0,0 +1,13 @@
+﻿int edad = 18;
+
+Console.WriteLine("Introduce tu edad");
+edad = Convert.ToInt32(Console.ReadLine());
+
+if (edad >= 18){
+    Console.WriteLine("Eres mayor de edad");
+    Console.WriteLine("ya puedes votar");
+}
+else{
+    Console.WriteLine("Eres menor de edad");
+    Console.WriteLine("no puedes votar");
+}
