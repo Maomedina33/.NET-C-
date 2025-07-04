465c516afc25172c6d90eb8c3ddbf8e9b84c29bc
Maomedina33 <ingmaomedina@gmail.com>
2025-07-04 12:30:13 -0500

    Add files via upload

---
+++ comandos/Interfaces/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+CalculatorService calculator = new CalculatorService();
+
+Console.WriteLine("Adition:"+ calculator.Add(5,3) );
+Console.WriteLine("Subtraction:" + calculator.Subtract(5, 3));
+Console.WriteLine("Multiplication:" + calculator.Multiply(5, 3));
+Console.WriteLine("Division:" + calculator.Divide(5, 3));
