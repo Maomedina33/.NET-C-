7d587f0d2d723846a8d547051d3ce91218dc3a4d
Maomedina33 <ingmaomedina@gmail.com>
2025-07-04 12:43:05 -0500

    Add files via upload

---
+++ comandos/Program6.cs
@@ -0,0 +1,13 @@
+﻿
+List<string> nombres2 = new List<string>();
+nombres2.Add("oscar");
+nombres2.Add("Felipe");
+nombres2.Add("Amin");
+nombres2.Add("Jorge");
+
+Console.WriteLine("Los nombres de mi lista son:");
+
+foreach (string nombre in nombres2)
+{
+    Console.WriteLine(nombre);
+} 
